3d1c54df9ac4a0f6c4be373f2a4d1157cf540f83
Hanvalad <hanvaladbabaev@yandex.ru>
2026-02-16 15:08:56 +0300

    test: configure H2 datasource for SpringBoot tests and fix contextLoads

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,7 +3,14 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:shareit;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.sql.init.mode=never"
+})
 class ShareItTests {
 
     @Test
